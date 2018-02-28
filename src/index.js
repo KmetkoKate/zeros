437bc25d4160bd55b9b86aef83da7b6818154f3e
KateKmetko <kmetkoekaterina@gmail.com>
2018-02-28 16:03:46 +0300

    implement function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  var count = 0,
+i  = 5;
+
+while(number/i != 0){
+
+count+=Math.floor(number/i);
+
+   i=i*5;
+
+   }
+return count;
 }
